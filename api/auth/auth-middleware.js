f9160accb3892e72c9235aa9b948c88224ea5cba
Tom Bielawski <70349084+tfbielawski@users.noreply.github.com>
2021-12-20 10:36:23 -0500

    Update auth-middleware.js

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,12 +1,7 @@
 const User = require("../users/users-model");
 
 /*
-  If the user does not have a session saved in the server
-
-  status 401
-  {
-    "message": "You shall not pass!"
-  }
+  If the user does not have a session saved in the server status 401
 */
 function restricted(req, res, next) {
     if(req.session.user) {next()}
@@ -14,13 +9,8 @@ function restricted(req, res, next) {
 }
 
 /*
-  If the username in req.body already exists in the database
-
-  status 422
-  {
-    "message": "Username taken"
-  }
-*/
+  If the username in req.body already exists in the database status 422
+ */
 async function checkUsernameFree(req, res, next) {
     try{
         const users = await User.findBy({username: req.body.username})
@@ -33,12 +23,7 @@ async function checkUsernameFree(req, res, next) {
 }
 
 /*
-  If the username in req.body does NOT exist in the database
-
-  status 401
-  {
-    "message": "Invalid credentials"
-  }
+  If the username in req.body does NOT exist in the database status 401
 */
 async function checkUsernameExists(req, res, next) {
     try{
@@ -56,12 +41,7 @@ async function checkUsernameExists(req, res, next) {
 
 
 /*
-  If password is missing from req.body, or if it's 3 chars or shorter
-
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
-  }
+  If password is missing from req.body, or if it's 3 chars or shorter status 422
 */
 function checkPasswordLength(req, res, next) {
     if(!req.body.password || req.body.password.length < 3){
